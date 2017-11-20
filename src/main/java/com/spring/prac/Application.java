ff096b5aa55503a00d46dd56ad9d254863ba7be1
AkhileshSingh <akhilesh.singh@target.com>
2017-11-20 12:21:48 +0530

    data jpa

---
+++ src/main/java/com/spring/prac/Application.java
@@ -0,0 +1,51 @@
+package com.spring.prac;
+
+import com.spring.prac.model.Customer;
+import com.spring.prac.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class Application {
+
+    public static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+
+    @Bean
+    public CommandLineRunner test(CustomerRepository customerRepository){
+        return (args)->{
+
+            customerRepository.save(new Customer("firsr1","last1"));
+            customerRepository.save(new Customer("firsr2","last2"));
+            customerRepository.save(new Customer("firsr3","last3"));
+            customerRepository.save(new Customer("firsr4","last4"));
+
+            //fetch all customer
+            logger.info("fetch all customer");
+            customerRepository.findAll().forEach(customer -> logger.info(customer.toString()));
+
+            logger.info("fetch customer by id");
+            Customer customer = customerRepository.findOne(1L);
+            logger.info(customer.toString());
+            logger.info(" ");
+
+
+            logger.info("fetch customer by last name");
+            List<Customer> customerList= customerRepository.findByLastName("last2");
+            customerList.forEach(customer1 -> logger.info(customer1.toString()));
+
+
+        };
+    }
+}
+
